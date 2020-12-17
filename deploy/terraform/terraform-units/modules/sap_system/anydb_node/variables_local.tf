93920ec4dc6c513f8b45448b121a1ae8e9046cb0
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-17 12:06:32 +0200

    Added the role back

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
@@ -224,12 +224,14 @@ dbnodes = local.anydb_ha ? (
         {
           name           = try("${dbnode.name}-0", format("%s%s%s%s", local.prefix, var.naming.separator, local.virtualmachine_names[idx], local.resource_suffixes.vm))
           computername   = try("${dbnode.name}-0", local.computer_names[idx], local.resource_suffixes.vm)
+          role           = try(dbnode.role, "db")
           admin_nic_ip   = lookup(dbnode, "admin_nic_ips", ["false", "false"])[0]
           db_nic_ip      = lookup(dbnode, "db_nic_ips", ["false", "false"])[0]
         },
         {
           name           = try("${dbnode.name}-1", format("%s%s%s%s", local.prefix, var.naming.separator, local.virtualmachine_names[idx + local.node_count], local.resource_suffixes.vm))
           computername   = try("${dbnode.name}-1", local.computer_names[idx + local.node_count])
+          role           = try(dbnode.role, "db")
           admin_nic_ip   = lookup(dbnode, "admin_nic_ips", ["false", "false"])[1]
           db_nic_ip      = lookup(dbnode, "db_nic_ips", ["false", "false"])[1]
         }
@@ -238,6 +240,7 @@ dbnodes = local.anydb_ha ? (
     flatten([for idx, dbnode in try(local.anydb.dbnodes, [{}]) : {
       name           = try("${dbnode.name}-0", format("%s%s%s%s", local.prefix, var.naming.separator, local.virtualmachine_names[idx], local.resource_suffixes.vm))
       computername   = try("${dbnode.name}-0", local.computer_names[idx], local.resource_suffixes.vm)
+      role           = try(dbnode.role, "db")
       admin_nic_ip   = lookup(dbnode, "admin_nic_ips", ["false", "false"])[0]
       db_nic_ip      = lookup(dbnode, "db_nic_ips", ["false", "false"])[0]
       }]
