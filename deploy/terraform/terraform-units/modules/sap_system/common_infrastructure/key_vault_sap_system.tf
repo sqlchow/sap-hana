f39d6ecf10a8b36e7584a485a2938d0a8b2f8223
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-29 01:23:36 +0200

    Always create SDU keyvaults

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/key_vault_sap_system.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/key_vault_sap_system.tf
@@ -25,7 +25,7 @@ data "azurerm_key_vault_secret" "sid_password" {
 
 // Create private KV with access policy
 resource "azurerm_key_vault" "sid_kv_prvt" {
-  count                      = local.enable_sid_deployment && local.sid_local_credentials_exist ? 1 : 0
+  count                      = local.enable_sid_deployment ? 1 : 0
   name                       = local.sid_keyvault_names.private_access
   location                   = local.region
   resource_group_name        = local.rg_exists ? data.azurerm_resource_group.resource_group[0].name : azurerm_resource_group.resource_group[0].name
@@ -48,7 +48,7 @@ resource "azurerm_key_vault" "sid_kv_prvt" {
 
 // Create user KV with access policy
 resource "azurerm_key_vault" "sid_kv_user" {
-  count                      = local.enable_sid_deployment && local.sid_local_credentials_exist ? 1 : 0
+  count                      = local.enable_sid_deployment ? 1 : 0
   name                       = local.sid_keyvault_names.user_access
   location                   = local.region
   resource_group_name        = local.rg_exists ? data.azurerm_resource_group.resource_group[0].name : azurerm_resource_group.resource_group[0].name
