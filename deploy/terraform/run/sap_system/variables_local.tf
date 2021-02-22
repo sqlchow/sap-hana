a77d7b1f40db0d2dc9f6ea668bba78961d86e105
Kimmo Forss <kimmo.forss@microsoft.com>
2021-02-22 15:34:49 +0200

    Reflect the count

--- deploy/terraform/run/sap_system/variables_local.tf
+++ deploy/terraform/run/sap_system/variables_local.tf
@@ -128,6 +128,6 @@ locals {
     client_id       = local.spn.client_id,
     client_secret   = local.spn.client_secret,
     tenant_id       = local.spn.tenant_id,
-    object_id       = data.azuread_service_principal.sp.id
+    object_id       = try(data.azuread_service_principal[0].sp.id, null)
   }
 }
