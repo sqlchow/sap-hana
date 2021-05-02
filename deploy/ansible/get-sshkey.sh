7d727cd5722a6c3d5d3e98a3181dd3437aab48e3
Kimmo Forss <kimmo.forss@microsoft.com>
2021-05-02 14:23:55 +0000

    Updated to reflect latest sap-parameters

--- deploy/ansible/get-sshkey.sh
+++ deploy/ansible/get-sshkey.sh
@@ -5,8 +5,9 @@
 #################################################################################
 
 kv=$(grep "kv_uri:" sap-parameters.yaml  | cut -d: -f2 | xargs )
-ksecret=$(grep "key_secret:" sap-parameters.yaml  | cut -d: -f2 | xargs )
+ksecret_prefix=$(grep "secret_prefix:" sap-parameters.yaml  | cut -d: -f2 | xargs )
 
+ksecret=$ksecret_prefix-sid-sshkey
 rm -f sshkey
 az keyvault secret show --vault-name ${kv} --name ${ksecret} | jq -r .value > sshkey
 chmod 600 sshkey
