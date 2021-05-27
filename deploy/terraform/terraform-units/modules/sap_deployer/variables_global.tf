07e60213a92b218324053a5cec3d3bb106f47f09
Kimmo Forss <kimmo.forss@microsoft.com>
2021-05-27 19:50:12 +0300

    Add the validation code to the module

--- deploy/terraform/terraform-units/modules/sap_deployer/variables_global.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/variables_global.tf
@@ -4,7 +4,40 @@ Description:
   Define input variables.
 */
 
-variable "infrastructure" {}
+variable "infrastructure" {
+  description = "Details of the Azure infrastructure to deploy the deployer into"
+  default     = {}
+
+  validation {
+    condition = (
+      length(trimspace(var.infrastructure.region)) != 0
+    )
+    error_message = "The region must be specified in the infrastructure.region field."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(var.infrastructure.environment)) != 0
+    )
+    error_message = "The environment must be specified in the infrastructure.environment field."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(try(var.infrastructure.vnets.management.arm_id, ""))) != 0 || length(trimspace(try(var.infrastructure.vnets.management.address_space, ""))) != 0
+    )
+    error_message = "Either the arm_id or address_space of the VNet must be specified in the infrastructure.vnets.management block."
+  }
+
+  validation {
+    condition = (
+      length(trimspace(try(var.infrastructure.vnets.management.subnet_mgmt.arm_id, ""))) != 0 || length(trimspace(try(var.infrastructure.vnets.management.subnet_mgmt.prefix, ""))) != 0
+    )
+    error_message = "Either the arm_id or prefix of the subnet must be specified in the infrastructure.vnets.management.subnet_mgmt block."
+  }
+
+}
+
 variable "deployers" {}
 variable "options" {}
 variable "ssh-timeout" {}
---
+++ documentation/SAP_Automation_on_Azure/Process_Documentation/WORKSPACES/SYSTEM/PROD-WUS2-SAP04-X04/terraform.tfvars
@@ -0,0 +1,3 @@
+# Custom disk size for the database backend
+
+db_disk_sizes_filename      = "X04-Disk_sizes.json"
