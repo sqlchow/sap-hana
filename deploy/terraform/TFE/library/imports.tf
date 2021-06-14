15c3fcda269bd19ea66a57a819c1dbb8acd3ab4e
Morgan Deegan <morgan.deegan@microsoft.com>
2021-06-14 15:15:48 -0700

    TFE: Adjustments

--- deploy/terraform/TFE/library/imports.tf
+++ deploy/terraform/TFE/library/imports.tf
@@ -1,41 +1,49 @@
-/*
-    Description:
-      Import deployer resources
-*/
-
-data "terraform_remote_state" "deployer" {
-  backend = "local"
-  count   = length(var.deployer_statefile_foldername) > 0 || local.use_deployer ? 1 : 0
+/*-----------------------------------------------------------------------------8
+|                                                                              |
+|                                 REMOTE STATE                                 |
+|                                                                              |
+| Description:                                                                 |
+|   Import deployer resources                                                  |
+|                                                                              |
++--------------------------------------4--------------------------------------*/
+data terraform_remote_state deployer {
+  backend                               = "local"
+  # count                                 = length(var.deployer_statefile_foldername) > 0 || local.use_deployer ? 1 : 0
   config = {
-    path = length(var.deployer_statefile_foldername)>0 ? "${var.deployer_statefile_foldername}/terraform.tfstate" :  "${abspath(path.cwd)}/../../LOCAL/${local.deployer_rg_name}/terraform.tfstate"
+    hostname                            = "app.terraform.io"
+    organization                        = "Azure_SAP_Automation"                # TODO: Kimmo
+    workspaces  = {
+      name                              = "MKD50-EUS2-DEP00-INFRASTRUCTURE"     # TODO: Kimmo
+    }
+    # path                                = length(var.deployer_statefile_foldername)>0 ? "${var.deployer_statefile_foldername}/terraform.tfstate" :  "${abspath(path.cwd)}/../../LOCAL/${local.deployer_rg_name}/terraform.tfstate"
   }
 }
 
-data "azurerm_key_vault_secret" "subscription_id" {
+data azurerm_key_vault_secret subscription_id {
   provider     = azurerm.deployer
   name         = format("%s-subscription-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "client_id" {
+data azurerm_key_vault_secret client_id {
   provider     = azurerm.deployer
   name         = format("%s-client-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "client_secret" {
+data azurerm_key_vault_secret client_secret {
   provider     = azurerm.deployer
   name         = format("%s-client-secret", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "tenant_id" {
+data azurerm_key_vault_secret tenant_id {
   provider     = azurerm.deployer
   name         = format("%s-tenant-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
 // Import current service principal
-data "azuread_service_principal" "sp" {
+data azuread_service_principal sp {
   application_id = local.spn.client_id
 }
--- deploy/terraform/bootstrap/sap_library/imports.tf
+++ deploy/terraform/bootstrap/sap_library/imports.tf
@@ -3,7 +3,7 @@
       Import deployer resources
 */
 
-data "terraform_remote_state" "deployer" {
+data terraform_remote_state deployer {
   backend = "local"
   count   = length(var.deployer_statefile_foldername) > 0 || local.use_deployer ? 1 : 0
   config = {
@@ -11,31 +11,31 @@ data "terraform_remote_state" "deployer" {
   }
 }
 
-data "azurerm_key_vault_secret" "subscription_id" {
+data azurerm_key_vault_secret subscription_id {
   provider     = azurerm.deployer
   name         = format("%s-subscription-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "client_id" {
+data azurerm_key_vault_secret client_id {
   provider     = azurerm.deployer
   name         = format("%s-client-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "client_secret" {
+data azurerm_key_vault_secret client_secret {
   provider     = azurerm.deployer
   name         = format("%s-client-secret", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
-data "azurerm_key_vault_secret" "tenant_id" {
+data azurerm_key_vault_secret tenant_id {
   provider     = azurerm.deployer
   name         = format("%s-tenant-id", upper(var.infrastructure.environment))
   key_vault_id = local.spn_key_vault_arm_id
 }
 
 // Import current service principal
-data "azuread_service_principal" "sp" {
+data azuread_service_principal sp {
   application_id = local.spn.client_id
 }
