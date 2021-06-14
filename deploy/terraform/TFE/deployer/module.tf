4943d35747d1d11307d27b73432ac566ff6d9b79
Morgan Deegan <morgan.deegan@microsoft.com>
2021-06-14 11:16:47 -0700

    TFE: Adjustments

--- deploy/terraform/TFE/deployer/module.tf
+++ deploy/terraform/TFE/deployer/module.tf
@@ -1,29 +1,33 @@
-/*
-Description:
-
-  Example to deploy deployer(s) using Terrafor Cloud / Enterprise.
-*/
+/*-----------------------------------------------------------------------------8
+|                                                                              |
+|                                    MODULE                                    |
+|                                                                              |
+| Description:                                                                 |
+|                                                                              |
+|   Example to deploy deployer(s) using Terrafor Cloud / Enterprise.           |
+|                                                                              |
++--------------------------------------4--------------------------------------*/
 module "sap_deployer" {
-  source                          = "../../terraform-units/modules/sap_deployer"
-  infrastructure                  = local.infrastructure
-  deployers                       = local.deployers
-  options                         = local.options
-  ssh-timeout                     = var.ssh-timeout
-  authentication                  = local.authentication
-  key_vault                       = local.key_vault
-  naming                          = module.sap_namegenerator.naming
-  firewall_deployment             = local.firewall_deployment
-  assign_subscription_permissions = local.assign_subscription_permissions
-  bootstrap                       = false
+  source                                = "../../terraform-units/modules/sap_deployer"
+  infrastructure                        = local.infrastructure
+  deployers                             = local.deployers
+  options                               = local.options
+  ssh-timeout                           = var.ssh-timeout
+  authentication                        = local.authentication
+  key_vault                             = local.key_vault
+  naming                                = module.sap_namegenerator.naming
+  firewall_deployment                   = local.firewall_deployment
+  assign_subscription_permissions       = local.assign_subscription_permissions
+  bootstrap                             = false
 }
 
 module "sap_namegenerator" {
-  source                          = "../../terraform-units/modules/sap_namegenerator"
-  environment                     = lower(local.infrastructure.environment)
-  deployer_environment            = lower(local.infrastructure.environment)
-  location                        = lower(local.infrastructure.region)
-  codename                        = lower(local.infrastructure.codename)
-  management_vnet_name            = local.vnet_mgmt_name_part
-  random_id                       = module.sap_deployer.random_id
-  deployer_vm_count               = local.deployer_vm_count
+  source                                = "../../terraform-units/modules/sap_namegenerator"
+  environment                           = lower(local.infrastructure.environment)
+  deployer_environment                  = lower(local.infrastructure.environment)
+  location                              = lower(local.infrastructure.region)
+  codename                              = lower(local.infrastructure.codename)
+  management_vnet_name                  = local.vnet_mgmt_name_part
+  random_id                             = module.sap_deployer.random_id
+  deployer_vm_count                     = local.deployer_vm_count
 }
