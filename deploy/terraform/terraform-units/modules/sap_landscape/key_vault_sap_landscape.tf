78b1308e9da59baf600919d10d8b08f6179cd2e2
Kimmo Forss <kimmo.forss@microsoft.com>
2021-06-24 21:19:29 +0300

    fix the regex

--- deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/key_vault_sap_landscape.tf
@@ -257,7 +257,7 @@ data "azurerm_key_vault_secret" "sid_password" {
 resource "azurerm_key_vault_secret" "witness_access_key" {
   provider     = azurerm.main
   count        = 1
-  name         = replace(format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.witness_accesskey),"/[^A-Za-z0-9]/-","")
+  name         = replace(format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.witness_accesskey),"/[^A-Za-z0-9-]/","")
   value        = length(var.witness_storage_account.arm_id) > 0 ? data.azurerm_storage_account.witness_storage[0].primary_access_key : azurerm_storage_account.witness_storage[0].primary_access_key
   key_vault_id = local.user_kv_exist ? local.user_key_vault_id : azurerm_key_vault.kv_user[0].id
 }
@@ -266,7 +266,7 @@ resource "azurerm_key_vault_secret" "witness_access_key" {
 resource "azurerm_key_vault_secret" "witness_name" {
   provider     = azurerm.main
   count        = 1
-  name         = replace(format("%s%s%s", local.prefix,  var.naming.separator, local.resource_suffixes.witness_name),"/[^A-Za-z0-9]/-","")
+  name         = replace(format("%s%s%s", local.prefix,  var.naming.separator, local.resource_suffixes.witness_name),"/[^A-Za-z0-9-]/","")
   value        = length(var.witness_storage_account.arm_id) > 0 ? data.azurerm_storage_account.witness_storage[0].name : azurerm_storage_account.witness_storage[0].name
   key_vault_id = local.user_kv_exist ? local.user_key_vault_id : azurerm_key_vault.kv_user[0].id
 }
