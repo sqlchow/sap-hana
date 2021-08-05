43ff14696247394081481f0f4a79399c1410e812
Kimmo Forss <kimmo.forss@microsoft.com>
2021-08-05 22:51:25 +0300

    remove duplicate

--- deploy/terraform/terraform-units/modules/sap_deployer/vm-deployer.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/vm-deployer.tf
@@ -34,21 +34,6 @@ resource "azurerm_network_interface" "deployer" {
   }
 }
 
-// User defined identity for all Deployer, assign contributor to the current subscription
-resource "azurerm_user_assigned_identity" "deployer" {
-  resource_group_name = local.rg_exists ? data.azurerm_resource_group.deployer[0].name : azurerm_resource_group.deployer[0].name
-  location            = local.rg_exists ? data.azurerm_resource_group.deployer[0].location : azurerm_resource_group.deployer[0].location
-  name                = format("%s%s", local.prefix, local.resource_suffixes.msi)
-}
-
-# // Add role to be able to deploy resources
-resource "azurerm_role_assignment" "sub_contributor" {
-  count                = var.assign_subscription_permissions && try(data.azurerm_client_config.deployer.object_id, "") != "" ? 1 : 0
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_user_assigned_identity.deployer.principal_id
-}
-
 // Linux Virtual Machine for Deployer
 resource "azurerm_linux_virtual_machine" "deployer" {
   count                           = length(local.deployers)
