0e3756bb423c2e5ea261a140dcf053e2d8f1eab1
Kimmo Forss <kimforss@microsoft.com>
2021-08-24 16:55:32 +0000

    Setup ADO

--- deploy/scripts/setup_ado.sh
+++ deploy/scripts/setup_ado.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
 function SetupAuthentication {
-    local organization="${1}"
-    local project="${2}"
-    local repoName="${3}"
-    local userName="${4}"
-    local PAT="${4}" 
+    if [ ! -n "${ORGANIZATION1}" ] ;then
 
-    # convert the Personal Access Token to a Base64 encoded string
-    B64Pat=$(echo $PAT | base64)
+    read -p "Please provide the Azure DevOps ORGANIZATION name? "  ORGANIZATION
+    export ORGANIZATION="${ORGANIZATION}"
+    echo "ORGANIZATION="${ORGANIZATION}>>/etc/environment
+    fi
 
-    # store the extra header for git to use
-    git config --global --add http.https://$userName@dev.azure.com/$organization/$project/_git/$repoName.extraHeader "AUTHORIZATION: Basic $B64Pat"
+    if [ ! -n "${PROJECT1}" ] ;then
+
+    read -p "Please provide the Azure DevOps PROJECT name? "  PROJECT
+    export PROJECT="${PROJECT}"
+    echo "PROJECT="${PROJECT}>>/etc/environment
+    fi
+
+    if [ ! -n "${REPONAME1}" ] ;then
+
+    read -p "Please provide the Azure DevOps repo name? "  REPONAME
+    export REPONAME="${REPONAME}"
+    echo "REPONAME="${REPONAME}>>/etc/environment
+    fi
+
+
+    if [ ! -n "${PAT1}" ] ;then
+
+    read -p "Please provide your Azure DevOps PAT? "  PAT
+    export PAT="${PAT}"
+    B64_PAT=$(printf "%s"":$PAT" | base64)
+    export B64_PAT="${B64_PAT}"
+    echo "B64_PAT="${B64_PAT}>>/etc/environment
+    echo "PAT="${PAT}>>/etc/environment
+    fi
+
 }
 
-read -p "Please provide the Azure DevOps organization name? "  organization
-read -p "Please provide the Azure DevOps project name? "  project
-read -p "Please provide the Azure DevOps repo name? "  repoName
-read -p "Please provide your Azure DevOps user name? "  userName
-read -p "Please provide your Azure DevOps PAT? "  PAT
+read -p "Register the environment variables? Y/N "  ans
+answer=${ans^^}
+if [ $answer == 'Y' ]; then
+    SetupAuthentication
+fi
 
-SetupAuthentication $organization $project $repoName $userName $PAT
 
-cd ~/Azure_SAP_Automated_Deployment
 
-B64_PAT=$(printf "%s"":$PAT" | base64)
-export B64_PAT=$B64_PAT
-git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://dev.azure.com/$organization/$project/_git/$repoName WORKSPACES
+cd /home/azureadm/Azure_SAP_Automated_Deployment || exit
+
+git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://dev.azure.com/$ORGANIZATION/$PROJECT/_git/$REPONAME WORKSPACES
