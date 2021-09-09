338857fdf65bd52987fc9edd9787da4190029e2c
hdamecharla <71097261+hdamecharla@users.noreply.github.com>
2021-09-09 13:10:42 +0530

    Update module.tf
    commenting out the provider aliases. The rest of the codes does not seem to realize the new aliases

--- deploy/terraform/bootstrap/sap_library/module.tf
+++ deploy/terraform/bootstrap/sap_library/module.tf
@@ -4,10 +4,11 @@
 */
 module "sap_library" {
   source                  = "../../terraform-units/modules/sap_library"
-  providers = {
-    azurerm.main     = azurerm
-    azurerm.deployer = azurerm.deployer
-  }
+# commenting out for fixing issues
+#  providers = {
+#    azurerm.main     = azurerm
+#    azurerm.deployer = azurerm.deployer
+#  }
   infrastructure          = local.infrastructure
   storage_account_sapbits = local.storage_account_sapbits
   storage_account_tfstate = local.storage_account_tfstate
