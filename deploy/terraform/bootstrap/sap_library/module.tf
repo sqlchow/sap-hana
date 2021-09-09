29e41f4f69204a9dacf9431ccdafc5ee40ca03e2
Kimmo Forss <kimforss@microsoft.com>
2021-09-09 07:26:10 +0000

    add both providers

--- deploy/terraform/bootstrap/sap_library/module.tf
+++ deploy/terraform/bootstrap/sap_library/module.tf
@@ -4,6 +4,10 @@
 */
 module "sap_library" {
   source                  = "../../terraform-units/modules/sap_library"
+  providers = {
+    azurerm.main     = azurerm
+    azurerm.deployer = azurerm.deployer
+  }
   infrastructure          = local.infrastructure
   storage_account_sapbits = local.storage_account_sapbits
   storage_account_tfstate = local.storage_account_tfstate
